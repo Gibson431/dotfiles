c366136f6ce60a42ba13fa312f10b4f3fc8c5675
Tim Gibson <timmaxgibson@gmail.com>
2024-03-02 18:36:01 +1100

    62 current  2024-03-02 18:26:35  24.05.20240228.9099616   6.7.6                           *

--- nixos/home-manager/default.nix
+++ nixos/home-manager/default.nix
@@ -1,8 +1,27 @@
 {...}: {
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
-  home-manager.users.main = import ./main.nix;
+  home-manager.users.main = {
+    imports = [
+      ./packages
+    ];
 
+    home.username = "main";
+    home.homeDirectory = "/home/main";
+
+    # This value determines the home Manager release that your
+    # configuration is compatible with. This helps avoid breakage
+    # when a new home Manager release introduces backwards
+    # incompatible changes.
+    #
+    # You can update home Manager without changing this value. See
+    # the home Manager release notes for a list of state version
+    # changes in each release.
+    home.stateVersion = "23.11";
+
+    # Let home Manager install and manage itself.
+    programs.home-manager.enable = true;
+  };
   # Optionally, use home-manager.extraSpecialArgs to pass
   # arguments to home.nix
 }
