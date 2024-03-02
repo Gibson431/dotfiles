8b291388d74ca18f3fd6031912f0e093b89ab45c
Tim Gibson <timmaxgibson@gmail.com>
2024-03-02 18:38:03 +1100

    62 current  2024-03-02 18:26:35  24.05.20240228.9099616   6.7.6                           *

--- nixos/nixos/packages/default.nix
+++ nixos/nixos/packages/default.nix
@@ -1,7 +1,28 @@
-{...}: {
-  imports = [
-    ./1password
-    ./neovim
-    ./steam
-  ];
+{
+  pkgs,
+  lib,
+  ...
+}:
+with lib; let
+  currentDir = ./.;
+  files = dir:
+    lib.attrsets.mapAttrsToList
+    (name: value:
+      if value == "directory"
+      then "${name}"
+      else null)
+    (builtins.readDir dir);
+
+  packages = dir: filter (file: ! isNull file) (files dir);
+
+  importDirectory = dir: let
+    dirPath = "${currentDir}/${dir}";
+  in
+    if builtins.pathExists dirPath
+    then import dirPath
+    else {};
+
+  importedPackages = builtins.map importDirectory (packages currentDir);
+in {
+  imports = importedPackages;
 }
