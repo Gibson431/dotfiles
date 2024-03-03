bcf8405d4135f33e988035c06f6443d0b346c524
Tim Gibson <timmaxgibson@gmail.com>
2024-03-03 17:42:22 +1100

    65 current  2024-03-03 17:41:51  24.05.20240228.9099616   6.7.6                           *

--- nixos/nixos/hardware-configuration.nix
+++ nixos/nixos/hardware-configuration.nix
@@ -41,4 +41,18 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = false;
+
+    # Enable the Nvidia settings menu,
+    # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
 }
