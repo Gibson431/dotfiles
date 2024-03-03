95178478e0a30f8441b63ab283e0d9046e9edff9
Tim Gibson <timmaxgibson@gmail.com>
2024-03-03 23:16:42 +1100

    71 current  2024-03-03 23:16:38  24.05.20240228.9099616   6.7.6                           *

--- .local/scripts/rebuild.bash
+++ .local/scripts/rebuild.bash
@@ -4,9 +4,9 @@ nvim .
 alejandra . &>/dev/null
 git diff -U0 *.nix
 echo "NixOS Rebuilding ... "
+git add .
 sudo nixos-rebuild switch --flake .
 # sudo nixos-rebuild switch --flake . &>nixos-switch.log || (cat nixos-switch.log | grep --color error && false)
 gen=$(nixos-rebuild list-generations | grep current)
-git add .
 git commit -am "$gen"
 popd
