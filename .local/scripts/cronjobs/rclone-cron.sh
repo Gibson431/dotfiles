2581557972eac998cb4877da71297239b1df5987
Tim Gibson <timmaxgibson@gmail.com>
2024-03-03 23:36:21 +1100

    73 current  2024-03-03 23:26:07  24.05.20240228.9099616   6.7.6                           *

--- .local/scripts/cronjobs/rclone-cron.sh
+++ .local/scripts/cronjobs/rclone-cron.sh
@@ -1,2 +1,4 @@
-#!/bin/bash
-/usr/bin/rclone bisync /home/main/Games/data/yuzu GoogleDrive: --log-file /home/main/.logs/rclone.log -vv
+#!/bin/sh
+mkdir -p /home/main/.logs
+touch /home/main/.logs/rclone.log
+rclone bisync /home/main/Games/data/yuzu GoogleDrive: --log-file /home/main/.logs/rclone.log -vv
