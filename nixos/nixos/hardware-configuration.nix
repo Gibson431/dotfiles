f7ad5aeecb2b5d3b1736474633139117b34fc1d9
Tim Gibson <timmaxgibson@gmail.com>
2024-03-13 18:57:28 +1100

    94 current  2024-03-13 18:56:52  23.11.20240304.617579a   6.7.7                           *

--- nixos/nixos/hardware-configuration.nix
+++ nixos/nixos/hardware-configuration.nix
@@ -46,6 +46,12 @@
     enable = true;
     driSupport = true;
     driSupport32Bit = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
   };
 
   services.xserver.videoDrivers = ["nvidiaBeta"];
