b996d69d48bfd60bf48cda78b9d6ed611d97e8f1
Tim Gibson <timmaxgibson@gmail.com>
2024-03-14 11:48:01 +1100

    95 current  2024-03-14 11:37:42  23.11.20240304.617579a   6.7.7                           *

--- .local/scripts/rebuild.bash
+++ .local/scripts/rebuild.bash
@@ -5,8 +5,15 @@ alejandra . &>/dev/null
 git diff -U0 *.nix
 echo "NixOS Rebuilding ... "
 git add .
-sudo nixos-rebuild switch --flake .
+git commit -am "temp commit"
+if sudo nixos-rebuild switch --flake . &>nixos-switch.log; then
+	gen=$(nixos-rebuild list-generations | grep current)
+	git reset HEAD~
+	git commit -am "$gen"
+else
+	git reset HEAD~
+	(cat nixos-switch.log | grep --color error && false)
+	false
+fi
 # sudo nixos-rebuild switch --flake . &>nixos-switch.log || (cat nixos-switch.log | grep --color error && false)
-gen=$(nixos-rebuild list-generations | grep current)
-git commit -am "$gen"
 popd
