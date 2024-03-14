7a5d05f4a3ba4be6a1cecc0494a87e6c40a2aff7
Gibson431 <timmaxgibson@gmail.com>
2024-03-14 13:17:28 +1100

    100 current  2024-03-14 13:15:59  23.11.20240304.617579a   6.7.7                           *

--- nixos/home-manager/packages/git/default.nix
+++ nixos/home-manager/packages/git/default.nix
@@ -5,20 +5,20 @@
   nixosConfig ? {},
   ...
 }: {
-  # programs.git = {
-  #   # package = pkgs.gitAndTools.gitFull;
-  #   enable = true;
-  #   userName = "Gibson431";
-  #   userEmail = "timmaxgibson@gmail.com";
-  #   ignores = ["*~" "*.swp"];
-  #   # includes = [{path = "~/.gitconfig.default";}];
-  #   extraConfig = {
-  #     core.editor = "nvim";
-  #     gpg.format = "ssh";
-  #     gpg."ssh".program = lib.mkIf (nixosConfig != {} && nixosConfig.programs._1password-gui.enable && nixosConfig.programs._1password-gui.sshAgent) "${pkgs._1password-gui}/bin/op-ssh-sign";
-  #     commit.gpgsign = true;
-  #     user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICgECjvqME2XcDTN8e9X9tGtj3yo4IU6rjm5m7SDo7dw";
-  #     push.autoSetupRemote = true;
-  #   };
-  # };
+  programs.git = {
+    # package = pkgs.gitAndTools.gitFull;
+    enable = true;
+    userName = "Gibson431";
+    userEmail = "timmaxgibson@gmail.com";
+    ignores = ["*~" "*.swp"];
+    # includes = [{path = "~/.gitconfig.default";}];
+    extraConfig = {
+      core.editor = "nvim";
+      gpg.format = "ssh";
+      gpg."ssh".program = lib.mkIf (nixosConfig != {} && nixosConfig.programs._1password-gui.enable && nixosConfig.programs._1password-gui.sshAgent) "${pkgs._1password-gui}/bin/op-ssh-sign";
+      commit.gpgsign = true;
+      user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICgECjvqME2XcDTN8e9X9tGtj3yo4IU6rjm5m7SDo7dw";
+      push.autoSetupRemote = true;
+    };
+  };
 }
