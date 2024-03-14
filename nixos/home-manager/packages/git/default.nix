b978b1d7b78f918c4be6d0788803bc0542ffbb27
Gibson431 <timmaxgibson@gmail.com>
2024-03-14 12:34:56 +1100

    97 current  2024-03-14 12:33:22  23.11.20240304.617579a   6.7.7                           *

--- nixos/home-manager/packages/git/default.nix
+++ nixos/home-manager/packages/git/default.nix
@@ -1,16 +1,24 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  config,
+  nixosConfig ? {},
+  ...
+}: {
   programs.git = {
-    package = pkgs.gitAndTools.gitFull;
+    # package = pkgs.gitAndTools.gitFull;
     enable = true;
-    userName = "Tim Gibson";
+    userName = "Gibson431";
     userEmail = "timmaxgibson@gmail.com";
+    ignores = ["*~" "*.swp"];
     # includes = [{path = "~/.gitconfig.default";}];
     extraConfig = {
       core.editor = "nvim";
       gpg.format = "ssh";
-      gpg."ssh".program = "${pkgs._1password-gui}/bin/op-ssh-sign";
+      gpg."ssh".program = lib.mkIf (nixosConfig != {} && nixosConfig.programs._1password-gui.enable && nixosConfig.programs._1password-gui.sshAgent) "${pkgs._1password-gui}/bin/op-ssh-sign";
       commit.gpgsign = true;
       user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICgECjvqME2XcDTN8e9X9tGtj3yo4IU6rjm5m7SDo7dw";
+      push.autoSetupRemote = true;
     };
   };
 }
--- nixos/nixos/packages/1password/default.nix
+++ nixos/nixos/packages/1password/default.nix
@@ -1,8 +1,20 @@
-{pkgs, ...}: {
-  programs._1password.enable = true;
-  programs._1password-gui = {
-    package = pkgs._1password-gui;
-    enable = true;
-    polkitPolicyOwners = ["main"];
+{
+  pkgs,
+  lib,
+  ...
+}:
+with lib; {
+  options.programs._1password-gui = {
+    sshAgent = mkEnableOption "1Password SSH Agent" // {default = true;};
+  };
+  config = {
+    programs = {
+      _1password.enable = true;
+      _1password-gui = {
+        package = pkgs._1password-gui;
+        enable = true;
+        polkitPolicyOwners = ["main"];
+      };
+    };
   };
 }
