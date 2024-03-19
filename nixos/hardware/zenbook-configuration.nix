af34cc38500edcfcf264dd5e3b2264d7279f1f79
Gibson431 <timmaxgibson@gmail.com>
2024-03-19 13:51:02 +1100

    7 current   2024-03-19 13:50:59  23.11.20240304.617579a   6.7.7                           *

--- nixos/hardware/zenbook-configuration.nix
+++ nixos/hardware/zenbook-configuration.nix
@@ -48,4 +48,8 @@
   environment.systemPackages = with pkgs; [
     fprintd
   ];
+
+  services.fprintd.enable = true;
+  services.fprintd.tod.enable = true;
+  services.fprintd.tod.driver = pkgs.libfprint-2-tod1-vfs0090;
 }
