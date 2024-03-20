d97db46152894e72c33c828d9bf2c723503d0902
Gibson431 <timmaxgibson@gmail.com>
2024-03-20 17:31:45 +1100

    10 current  2024-03-20 16:20:09  23.11.20240304.617579a   6.7.7                           *

--- nixos/nixos/packages/gnome/default.nix
+++ nixos/nixos/packages/gnome/default.nix
@@ -10,5 +10,27 @@
   ];
 in {
   environment.systemPackages = gnome ++ extensions;
+
+  # environment.gnome.excludePackages =
+  #   (with pkgs; [
+  #     gnome-photos
+  #     gnome-tour
+  #   ])
+  #   ++ (with pkgs.gnome; [
+  #     cheese # webcam tool
+  #     gnome-music
+  #     gnome-terminal
+  #     gedit # text editor
+  #     epiphany # web browser
+  #     geary # email reader
+  #     evince # document viewer
+  #     gnome-characters
+  #     totem # video player
+  #     tali # poker game
+  #     iagno # go game
+  #     hitori # sudoku game
+  #     atomix # puzzle game
+  #   ]);
+
   # services.udev.packages = with pkgs; [gnome.gnome-settings-daemon];
 }
