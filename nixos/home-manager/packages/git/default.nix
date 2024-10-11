932f74772964ef52b03ebb3a547252ec6efad874
Gibson431 <timmaxgibson@gmail.com>
2024-10-12 01:33:26 +1100

    update ssh signing key

--- nixos/home-manager/packages/git/default.nix
+++ nixos/home-manager/packages/git/default.nix
@@ -13,11 +13,11 @@
     ignores = ["*~" "*.swp"];
     # includes = [{path = "~/.gitconfig.default";}];
     extraConfig = {
-      core.editor = "nvim";
+      core.editor = "hx";
       gpg.format = "ssh";
       gpg."ssh".program = lib.mkIf (nixosConfig != {} && nixosConfig.programs._1password-gui.enable && nixosConfig.programs._1password-gui.sshAgent) "${pkgs._1password-gui}/bin/op-ssh-sign";
       commit.gpgsign = true;
-      user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICgECjvqME2XcDTN8e9X9tGtj3yo4IU6rjm5m7SDo7dw";
+      user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEtaYVm8RCmHQCPhvfzJwxpF0rXGBBR5n27aRL1rAdXN";
       push.autoSetupRemote = true;
     };
   };
