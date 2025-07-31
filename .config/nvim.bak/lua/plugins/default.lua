9f0eb22c9c449cf7b1be387db88b5b455e40d434
Gibson431 <timmaxgibson@gmail.com>
2025-07-31 13:15:57 +1000

    use native package manager and simple config

---
+++ .config/nvim.bak/lua/plugins/default.lua
@@ -0,0 +1,164 @@
+return {
+  { 'numToStr/Comment.nvim' },
+  { -- Adds git related signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+    },
+  },
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    main = 'ibl',
+    opts = {},
+  },
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    opts = {},
+    config = function()
+      vim.cmd.colorscheme 'catppuccin-frappe'
+    end,
+  },
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {},
+  },
+  {
+    'nvim-telescope/telescope.nvim',
+    opts = {
+      defaults = {
+        layout_strategy = 'horizontal',
+        layout_config = { prompt_position = 'top' },
+        sorting_strategy = 'ascending',
+        winblend = 0,
+      },
+    },
+  },
+  {
+    'hrsh7th/nvim-cmp',
+    version = false, -- last release is way too old
+    event = 'InsertEnter',
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+    },
+    -- Not all LSP servers add brackets when completing a function.
+    -- To better deal with this, LazyVim adds a custom option to cmp,
+    -- that you can configure. For example:
+    --
+    -- ```lua
+    -- opts = {
+    --   auto_brackets = { "python" }
+    -- }
+    -- ```
+    opts = function()
+      vim.api.nvim_set_hl(0, 'CmpGhostText', { link = 'Comment', default = true })
+      local cmp = require 'cmp'
+      local defaults = require 'cmp.config.default'()
+      local auto_select = true
+      return {
+        auto_brackets = {}, -- configure any filetype to auto add brackets
+        completion = {
+          completeopt = 'menu,menuone,noinsert' .. (auto_select and '' or ',noselect'),
+        },
+        preselect = auto_select and cmp.PreselectMode.Item or cmp.PreselectMode.None,
+        mapping = cmp.mapping.preset.insert {
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-n>'] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+          ['<C-p>'] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
+          ['<C-Space>'] = cmp.mapping.complete(),
+          -- ['<CR>'] = LazyVim.cmp.confirm { select = auto_select },
+          -- ['<C-y>'] = LazyVim.cmp.confirm { select = true },
+          -- ['<S-CR>'] = LazyVim.cmp.confirm { behavior = cmp.ConfirmBehavior.Replace }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ['<C-CR>'] = function(fallback)
+            cmp.abort()
+            fallback()
+          end,
+          -- ['<tab>'] = function(fallback)
+          --   return LazyVim.cmp.map({ 'snippet_forward', 'ai_accept' }, fallback)()
+          -- end,
+        },
+        sources = cmp.config.sources({
+          { name = 'lazydev' },
+          { name = 'nvim_lsp' },
+          { name = 'path' },
+        }, {
+          { name = 'buffer' },
+        }),
+        formatting = {
+          format = function(entry, item)
+            -- local icons = LazyVim.config.icons.kinds
+            -- if icons[item.kind] then
+            --   item.kind = icons[item.kind] .. item.kind
+            -- end
+
+            local widths = {
+              abbr = vim.g.cmp_widths and vim.g.cmp_widths.abbr or 40,
+              menu = vim.g.cmp_widths and vim.g.cmp_widths.menu or 30,
+            }
+
+            for key, width in pairs(widths) do
+              if item[key] and vim.fn.strdisplaywidth(item[key]) > width then
+                item[key] = vim.fn.strcharpart(item[key], 0, width - 1) .. '…'
+              end
+            end
+
+            return item
+          end,
+        },
+        experimental = {
+          -- only show ghost text when we show ai completions
+          ghost_text = vim.g.ai_cmp and {
+            hl_group = 'CmpGhostText',
+          } or false,
+        },
+        sorting = defaults.sorting,
+      }
+    end,
+  },
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'rcarriga/nvim-notify',
+    },
+    opts = {
+      cmdline = {
+        view = 'cmdline',
+      },
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+          ['vim.lsp.util.stylize_markdown'] = true,
+          ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+        },
+      },
+      presets = { command_pallete = false },
+    },
+  },
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    -- Optional dependency
+    dependencies = { 'hrsh7th/nvim-cmp' },
+    config = function()
+      require('nvim-autopairs').setup {}
+      -- If you want to automatically add `(` after selecting a function or method
+      local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+      local cmp = require 'cmp'
+      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+    end,
+  },
+}
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,99 @@
-require 'config.default'
+vim.g.mapleader = ' '
+
+-- opts
+vim.opt.number = true
+vim.opt.wrap = true
+vim.opt.tabstop = 4
+vim.opt.swapfile = false
+vim.opt.shiftwidth = 0
+vim.opt.signcolumn = 'yes'
+vim.opt.breakindent = true
+vim.opt.undofile = true
+vim.opt.updatetime = 0
+vim.opt.cursorline = true
+vim.opt.winborder = 'rounded'
+vim.opt.scrolloff = 7
+
+-- packages
+vim.pack.add {
+  { src = 'https://github.com/catppuccin/nvim' },
+  { src = 'https://github.com/echasnovski/mini.nvim' },
+  { src = 'https://github.com/neovim/nvim-lspconfig' },
+  { src = 'https://github.com/mason-org/mason.nvim' },
+  { src = 'https://github.com/stevearc/oil.nvim' },
+  { src = 'https://github.com/folke/which-key.nvim' },
+  { src = 'https://github.com/lewis6991/gitsigns.nvim' },
+  { src = 'https://github.com/lukas-reineke/indent-blankline.nvim' },
+  { src = 'https://github.com/nvim-lualine/lualine.nvim' },
+  { src = 'https://github.com/stevearc/conform.nvim' },
+  { src = 'https://github.com/rachartier/tiny-inline-diagnostic.nvim' },
+}
+
+vim.cmd.colorscheme 'catppuccin-frappe'
+
+require('mini.basics').setup()
+require('mini.extra').setup()
+require('mini.completion').setup()
+require('mini.pick').setup()
+require('mini.cursorword').setup()
+require('mini.pairs').setup()
+require('mini.icons').setup()
+require('mini.indentscope').setup { options = { try_as_border = true } }
+require('mason').setup()
+require('ibl').setup()
+require('lualine').setup()
+require('tiny-inline-diagnostic').setup { preset = 'simple', show_all_diags_on_cursorline = true }
+require('gitsigns').setup {
+  signs = {
+    add = { text = '+' },
+    change = { text = '~' },
+    delete = { text = '_' },
+    topdelete = { text = '‾' },
+    changedelete = { text = '~' },
+  },
+}
+require('conform').setup {
+  notify_on_error = false,
+  formatters_by_ft = {
+    lua = { 'stylua' },
+    cpp = { 'clang-format' },
+    rust = { 'rust_analyzer' },
+  },
+}
+
+-- lsp
+vim.lsp.enable { 'lua_ls', 'shellcheck', 'clangd', 'rust_analyzer' }
+
+-- keymaps
+require('which-key').add {
+  { '<leader>e', '<cmd>Explore<cr>', desc = 'Oil' },
+  {
+    group = 'Lsp',
+    { '<leader>l', group = 'Lsp' },
+    { '<leader>lf', '<cmd>Format<cr>', desc = 'Lsp Format' },
+    { '<leader>lr', vim.lsp.buf.rename, desc = 'Lsp Rename' },
+    { '<leader>la', vim.lsp.buf.code_action, desc = 'Lsp Action' },
+  },
+  {
+    group = 'Search',
+    { '<leader>s', group = 'Search' },
+    { '<leader>sf', '<cmd>Pick files<cr>', desc = 'Search Files' },
+    { '<leader>sb', '<cmd>Pick buffers<cr>', desc = 'Search Buffers' },
+  },
+  { '<esc><esc>', '<cmd>nohlsearch<cr>', silent = true },
+  { 'gD', vim.lsp.buf.declaration, desc = 'Declaration' },
+  { 'gd', vim.lsp.buf.definition, desc = 'Definition' },
+}
+
+-- cmds
+vim.api.nvim_create_user_command('Format', function(args)
+  local range = nil
+  if args.count ~= -1 then
+    local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+    range = {
+      start = { args.line1, 0 },
+      ['end'] = { args.line2, end_line:len() },
+    }
+  end
+  require('conform').format { async = true, lsp_format = 'fallback', range = range }
+end, { range = true })
